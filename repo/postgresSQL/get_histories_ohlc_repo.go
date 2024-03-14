0c5f171c6f7a6f73ff5c8b6b381763fb000d70cd
MinhSang97 <minhsangnguyen463@gmail.com>
2024-03-14 21:59:06 +0700

    GO-Rest-Repository

--- repo/postgresSQL/get_histories_ohlc_repo.go
+++ repo/postgresSQL/get_histories_ohlc_repo.go
@@ -20,16 +20,21 @@ var RedisClient = redis.ConnectRedis()
 
 func (s ohldDataRepository) GetHistories(ctx context.Context, startDate int64, endDate int64, period, symbol string) ([]model.OHLCData, error) {
 	var ohldData []model.OHLCData
+	fmt.Println(endDate)
 
 	switch period {
 	case "1D":
-		var ohlcData []model.OHLCData
-		var count int
-		err := s.db.Raw("SELECT * FROM ohlc_data_1day WHERE timestamp = ? OR timestamp = ?", startDate, endDate).Error
-		if err != nil || count != 2 {
-			log.Println("End Time not available. Need call API get data")
-			return ohlcData, fmt.Errorf("End Time not available. Need call API get data")
+		var count int64
+		err := s.db.Raw("SELECT count(*) FROM ohlc_data_1day WHERE timestamp = ? OR timestamp = ?", startDate, endDate).Count(&count).Error
 
+		// Kiểm tra lỗi và số lượng kết quả
+		if err != nil || count != 2 {
+			errMsg := "End Time not available. Need to call API to get data"
+			if err != nil {
+				errMsg += ": " + err.Error()
+			}
+			log.Println(errMsg)
+			return ohldData, fmt.Errorf("%s", errMsg)
 		} else {
 			cacheKey := fmt.Sprintf("ohlc_data:%s:%d:%d", symbol, startDate, endDate)
 
